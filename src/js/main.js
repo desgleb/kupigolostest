7b9da6674b54a848cc3f83a77cb1ab1a5fe4fa63
Gleb Deshin <desgleb@gmail.com>
2022-06-10 11:44:39 +0300

    bottom menu logic done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const headerLinks = document.querySelectorAll('.header__bottom-link');
+  const headerDropdowns = document.querySelectorAll('.header__bottom-dropdown');
+
+  document.body.addEventListener('click', e => {
+    let target = e.target;
+    headerLinks.forEach(link => {
+      // let linkParent = link.parentElement;
+      headerDropdowns.forEach(dd => {
+        if (target !== link && target !== dd) {
+          dd.classList.remove('dropdown--visible');
+        }
+      });
+    });
+  });
+
+  headerLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      headerDropdowns.forEach(dd => {
+        dd.classList.remove(visibleClass);
+      });
+
+      const visibleClass = 'dropdown--visible';
+
+      let target = e.target;
+      let targetDropdown = target.nextSibling.nextSibling;
+
+      headerDropdowns.forEach(dd => {
+        if (dd == targetDropdown && !dd.classList.contains(visibleClass)) {
+          dd.classList.add(visibleClass);
+        } else {
+          dd.classList.remove(visibleClass);
+        }
+      });
+    });
+
+    link.addEventListener('mouseover', (e) => {
+      headerDropdowns.forEach(dd => {
+        dd.classList.remove(visibleClass);
+      });
+
+      const visibleClass = 'dropdown--visible';
+
+      let target = e.target;
+      let targetDropdown = target.nextSibling.nextSibling;
+
+      headerDropdowns.forEach(dd => {
+        if (dd == targetDropdown && !dd.classList.contains(visibleClass)) {
+          dd.classList.add(visibleClass);
+        } else {
+          dd.classList.remove(visibleClass);
+        }
+      });
+    });
+  });
+
 
 });
